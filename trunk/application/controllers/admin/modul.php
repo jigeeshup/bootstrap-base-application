58135d42bdd9a86b3979ccd49864151f2671aa4c
joharijumali@gmail.com <joharijumali@gmail.com@df177050-2a58-d094-e041-41980c87fb7e>
2013-04-29 06:49:03 +0000

    new setup pages

--- trunk/application/controllers/admin/modul.php
+++ trunk/application/controllers/admin/modul.php
@@ -10,9 +10,35 @@ class Admin_Modul_Controller extends Base_Controller {
 
     public function get_application(){
 
-    	return View::make('admin.modul.application');
+    	$flow = New Arcone;
+
+        $data['struct'] = Menutree::generate();
+
+    	return View::make('admin.modul.application',$data);
+
+    }
+
+    public function post_application(){
+
+        $contents = Input::all();
+
+        Admin_Modul::setPages($contents);
+
+        return Redirect::to('admin/modul/application');
 
     }	
+
+    public function get_register(){
+
+        $structure = New Setup;
+
+        $data['struct'] = $structure->populatemodul();
+
+        return View::make('admin.modul.register',$data);
+
+    }
+
+
 
 }
 
