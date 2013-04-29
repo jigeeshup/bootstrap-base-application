cb9c8e75770c529cd576515e826afba1abba3d01
joharijumali <joharijumali@gmail.com>
2013-04-29 06:50:28 +0000

    add pages setup view

--- application/views/admin/modul/application.blade.php
+++ application/views/admin/modul/application.blade.php
@@ -2,6 +2,16 @@
 
 @section('content')
 <div class="page-header">
-    <h3>Application List</h3>
+    <h3>Pages Setup</h3>
+</div>
+<div class="row-fluid">
+  <div class="span12">
+    {{ Form::open('admin/modul/application', 'POST') }}
+    {{ $struct }}
+    <div class="form-actions">
+	    <button type="submit" class="btn pull-right btn-primary">Save changes</button>
+  	</div>
+    {{ Form::close()}}
+  </div>
 </div>
 @endsection
